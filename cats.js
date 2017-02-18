22d281b85357942a93eb236938e741fd0acd4299
Graham Moss <graham.moss@hotmail.com>
2017-02-18 14:38:15 -0500

    Complete assignment

--- cats.js
+++ cats.js
@@ -2,13 +2,23 @@ $(document).ready(function() {
 
   $(".summon-cats").on('click', function() {
 
-    console.log('hello');
+    $.ajax ({
+      url: 'http://bitkittens.herokuapp.com/cats.json',
+      dataType: 'json'
 
+    }).done(function(responseData) {
+      console.log(responseData);
+
+      for (var i=0; i < responseData.cats.length; i++) {
+
+        $('<img>').attr({
+          src: responseData.cats[i].photo,
+          alt: 'Photo of ' +  responseData.cats[i].name }).appendTo('#cat'+ (i+1));
+
+      }
+
+    });
 
   });
-
-
-
-
 
 });
